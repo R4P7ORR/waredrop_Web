91331249fb3a6aa91a86819957b791a7edcd663f
Darkkhalys <145651320+Darkkhalys@users.noreply.github.com>
2024-04-11 10:53:18 +0200

    Delete WareDrop_Backend/waredrop_backend/dist directory

--- WareDrop_Backend/waredrop_backend/dist/auth/guards/permission.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-import { Permission } from "../../permissions/permissions.service";
-export declare const RequiredPermission: import("@nestjs/core").ReflectableDecorator<Permission[], Permission[]>;
