71c1ac6becf76c88df721e02e2101c494d869071
Hunor Donkó <donko.hunor.attila@gmail.com>
2024-04-18 13:38:33 +0200

    Completed backend documentation with Swagger

--- WareDrop_Backend/waredrop_backend/src/permissions/permissions.controller.ts
+++ WareDrop_Backend/waredrop_backend/src/permissions/permissions.controller.ts
@@ -3,8 +3,15 @@ import {AssignPermissionDto, Permission, PermissionDto, PermissionsService} from
 import {JwtAuthGuard} from "../auth/guards/jwt.guard";
 import {PermissionGuard} from "../auth/guards/permission.guard";
 import {RequiredPermission} from "../auth/guards/permission.decorator";
-import {UserDto} from "../users/users.service";
-import {ApiTags} from "@nestjs/swagger";
+import {
+    ApiBadRequestResponse,
+    ApiBearerAuth,
+    ApiForbiddenResponse,
+    ApiOkResponse,
+    ApiOperation, ApiParam,
+    ApiTags,
+    ApiUnauthorizedResponse
+} from "@nestjs/swagger";
 
 @ApiTags('Permissions')
 @Controller('permissions')
@@ -12,36 +19,72 @@ import {ApiTags} from "@nestjs/swagger";
 export class PermissionsController {
     constructor(private readonly service: PermissionsService,) { }
 
+    @ApiOperation({summary: 'Create permission (Admin)', description: 'Creates a new permission in the database'})
+    @ApiOkResponse({description: 'Permission created'})
+    @ApiBadRequestResponse({description: 'In the error message, it says which value failed the validation and why'})
+    @ApiUnauthorizedResponse({description: 'The request came from an unauthorized user or the token expired'})
+    @ApiForbiddenResponse({description: 'The user is not an admin'})
+    @ApiBearerAuth()
     @Post()
     @RequiredPermission([{permissionName: 'All'}])
     createNewPermission(@Body() newPermission: Permission){
         return this.service.createPermission(newPermission);
     }
 
+    @ApiOperation({summary: 'User permission (Admin)', description: `Returns a user's permission(s)`})
+    @ApiOkResponse({description: 'Returned the permission(s)'})
+    @ApiUnauthorizedResponse({description: 'The request came from an unauthorized user or the token expired'})
+    @ApiForbiddenResponse({description: 'The user is not an admin'})
+    @ApiParam({description: 'Id of a user', type: 'string', name: 'id'})
+    @ApiBearerAuth()
     @Get('/:id')
     @RequiredPermission([{permissionName: 'All'}])
-    getPermissionsByUser(@Param() userId: UserDto){
-        return this.service.getPermissionsByUser(userId);
+    getPermissionsByUser(@Param() userIdString: string){
+        const userId: number = parseInt(userIdString);
+        return this.service.getPermissionsByUser({userId: userId});
     }
 
+    @ApiOperation({summary: 'All permissions (Admin)', description: 'Returns all permissions'})
+    @ApiOkResponse({description: 'Returned all permission(s)'})
+    @ApiUnauthorizedResponse({description: 'The request came from an unauthorized user or the token expired'})
+    @ApiForbiddenResponse({description: 'The user is not an admin'})
+    @ApiBearerAuth()
     @Get()
     @RequiredPermission([{permissionName: 'All'}])
     listPermissions(){
         return this.service.getAllPermissions();
     }
 
+    @ApiOperation({summary: 'Give permission (Admin)', description: 'Assigns a permission to a role'})
+    @ApiOkResponse({description: 'Permission assigned'})
+    @ApiBadRequestResponse({description: 'In the error message, it says which value failed the validation and why'})
+    @ApiUnauthorizedResponse({description: 'The request came from an unauthorized user or the token expired'})
+    @ApiForbiddenResponse({description: 'The user is not an admin'})
+    @ApiBearerAuth()
     @Patch()
     @RequiredPermission([{permissionName: 'All'}])
     givePermission(@Body() assignInput: AssignPermissionDto){
         return this.service.givePermission(assignInput);
     }
 
+    @ApiOperation({summary: 'Revoke permission (Admin)', description: 'Revoke a permission from a role'})
+    @ApiOkResponse({description: 'Permission revoked'})
+    @ApiBadRequestResponse({description: 'In the error message, it says which value failed the validation and why'})
+    @ApiUnauthorizedResponse({description: 'The request came from an unauthorized user or the token expired'})
+    @ApiForbiddenResponse({description: 'The user is not an admin'})
+    @ApiBearerAuth()
     @Patch('/remove')
     @RequiredPermission([{permissionName: 'All'}])
     removePermission(@Body() removeInput: AssignPermissionDto){
         return this.service.removePermission(removeInput);
     }
 
+    @ApiOperation({summary: 'Delete permission (Admin)', description: 'Deletes a permission from the database'})
+    @ApiOkResponse({description: 'Permission deleted'})
+    @ApiBadRequestResponse({description: 'In the error message, it says which value failed the validation and why'})
+    @ApiUnauthorizedResponse({description: 'The request came from an unauthorized user or the token expired'})
+    @ApiForbiddenResponse({description: 'The user is not an admin'})
+    @ApiBearerAuth()
     @Delete()
     @RequiredPermission([{permissionName: 'All'}])
     deletePermission(@Body() deletePermission: PermissionDto){
